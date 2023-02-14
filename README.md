d6dc3b22968eaa6b5525c5c1c9a85e1bfad8402c
Edoardo Cignoni <edoardo.cignoni96@gmail.com>
2023-02-14 15:50:26 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Implementation of sTDA in PyTorch
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="stda_torch",
+    version="0.0.1",
+    url="https://github.com/ecignoni/stda_torch",
+    author="Edoardo Cignoni",
+    author_email="edoardo.cignoni96@gmail.com",
+    description=open("README.md").read(),
+    packages=["stda_torch"],
+)
+
